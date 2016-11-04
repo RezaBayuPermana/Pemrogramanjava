c330056a48fbe08c91f830f160681c152d6d7bde
Rheza Permana <Rheza Permana@RhezaPermana-PC>
2016-11-04 18:00:19 +0700

    12141378 etika looping for i=0 maka akan dicek dengan kondisi kurang dari panjang. Panjang didapatkan dari inputan parameter. Jika memenuhi maka akan masuk ke looping for j. Pada for j mulai dari j=0 dengan batas j<i, hal ini bertujuan agar pada baris pertama hanya menampilkan 1 bintang dan akan bertambah 1 bintang pada bariss dibawahnya. Dalam for j, bintang akan tercetak dari kiri ke kanan tidak akan ke bawah karena menggunakan sout. Setelah mencetak bintang maka j++ dan mulai for j lagig dengan nilai j sekarang 1 karena melebihi batas maka for j berhenti dan akan dienter karena ada perintah sout. Kemudian for pada i akan di i++ menjadi i=1. Hal ini akan terus dilakukan selama for i belum mencapai batas panjang.

--- src/PemrogramanJava1.java
+++ src/PemrogramanJava1.java
@@ -8,3 +8,10 @@ class PemrogramanJava1 {
         System.out.printf("Jumlah Karakter/Tinggi Segitiga : ");
         panjang = input.nextInt();
         System.out.print("-----------------------------------");
+        // Segitiga siku kiri atas      
+        for(i=0;i<=panjang;i++) {
+            for(j=0; j<i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
