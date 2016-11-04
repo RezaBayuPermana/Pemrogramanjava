05780a8c5a1538bfcc35043a0549469f723d3c35
Rheza Permana <Rheza Permana@RhezaPermana-PC>
2016-11-04 17:59:05 +0700

    12141378 user ketika program dijalankan (runtime). Untuk dapat menampung inputan ketika program dijalankan, menggunakan bantuan Scanner. Setelah dilakukan proses instansiasi terhadap Scanner, baru dapat menggunakan fungsi nextInt() untuk menangkap nilai Integer (inputan nilai panjang).

---
+++ src/PemrogramanJava1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class PemrogramanJava1 {
+    public static void main(String[] args) {
+        int i, j;
+        int panjang;
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Jumlah Karakter/Tinggi Segitiga : ");
+        panjang = input.nextInt();
+        System.out.print("-----------------------------------");
